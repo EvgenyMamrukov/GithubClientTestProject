401deb998373a79f2000dc7abf05637b50039999
Evgeny Mamrukov <mamevg@mail.ru>
2018-04-19 12:08:40 +0600

    refresh repositories event has been added

--- GithubClient/app/src/main/java/com/example/evgeny/githubclient/api/services/events/EventIds.java
+++ GithubClient/app/src/main/java/com/example/evgeny/githubclient/api/services/events/EventIds.java
@@ -8,4 +8,5 @@ public class EventIds {
     public static final long LOGIN_EVENT = 1;
     public static final long GET_REPOSITORIES_EVENT = 2;
     public static final long LOGOUT_EVENT = 3;
+    public static final long REFRESH_REPOSITORIES_EVENT = 4;
 }
