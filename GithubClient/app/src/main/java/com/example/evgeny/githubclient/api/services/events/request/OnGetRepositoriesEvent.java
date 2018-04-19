6477457240b441f36107dd7b177b9a51ded8aa60
Evgeny Mamrukov <mamevg@mail.ru>
2018-04-19 12:10:14 +0600

    on get repositories event has been updated

--- GithubClient/app/src/main/java/com/example/evgeny/githubclient/api/services/events/request/OnGetRepositoriesEvent.java
+++ GithubClient/app/src/main/java/com/example/evgeny/githubclient/api/services/events/request/OnGetRepositoriesEvent.java
@@ -7,10 +7,14 @@ package com.example.evgeny.githubclient.api.services.events.request;
 public class OnGetRepositoriesEvent extends RequestEvent {
 
     private String query;
+    private int page;
+    private int perPage;
 
-    public OnGetRepositoriesEvent(long requestCode, String query) {
+    public OnGetRepositoriesEvent(long requestCode, String query, int page, int perPage) {
         super(requestCode);
         this.query = query;
+        this.page = page;
+        this.perPage = perPage;
     }
 
     public String getQuery() {
@@ -19,5 +23,21 @@ public class OnGetRepositoriesEvent extends RequestEvent {
 
     public void setQuery(String query) {
         this.query = query;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(int perPage) {
+        this.perPage = perPage;
     }
 }
